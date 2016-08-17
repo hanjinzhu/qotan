f7c4b229fffeb2e4eb11aa7eda993c2b4dc516fe
linxin <linxin@movee.cn>
2016-08-17 10:12:16 +0800

    c

--- static/js/register.js
+++ static/js/register.js
@@ -1,4 +1,5 @@
 (function () {
+	$(".email").focus();
 	$(".email").focusout(function(){
 		var self = $(this);
 		var email = self.val();
@@ -41,4 +42,31 @@
 		  	}
 		},"json");
 	});
+
+	$(".submit_register").click(function(){
+		$.get("/user/doregister", {email:$(".email").val(),nick:$(".nick").val(),password:$(".password").val()},function(data){
+		  	if(data['code'] == 0){
+		  		$(".register_success_info").show();
+		  		$(".login-form-section").hide();
+		  		var intervalid = setInterval("clock()", 1000);
+		  	}else{
+		  		if(typeof(data['error_type'])!="undefined"){
+		  			var error_type = data['error_type'];
+		  			$("."+error_type).parent().css("border","1px solid #d89090");
+			  		$("."+error_type).parent().parent().find(".register_alert").remove();
+			  		$("."+error_type).parent().parent().append('<span class="register_alert">'+data.msg+'</span>');
+		  		}
+		  	}
+		},"json");
+	});
 })();
+
+var sec = 5; 
+function clock() { 
+	if (sec == 0) { 
+		window.location.href="/home";
+		clearInterval(intervalid); 
+	} 
+	$("#mes").text(sec); 
+	sec--; 
+}  
