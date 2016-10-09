232b08406fe52b29658615552e9acb785dfc469a
linxin <601630504@qq.com>
2016-10-09 18:23:39 +0800

    comit

--- application/views/ucenter.php
+++ application/views/ucenter.php
@@ -26,9 +26,7 @@ $this->load->view('common/header');
     <div class="white_pannel" style="margin-top:20px">
         <ul id="myTab" class="nav nav-tabs">
             <li class="active"><a href="#dynamic" data-toggle="tab">我的动态</a></li>
-            <li><a href="#like" data-toggle="tab">我喜欢的文章</a></li>
-            <li><a href="#follow" data-toggle="tab">我的关注</a></li>
-            <li><a href="#fans" data-toggle="tab">我的粉丝</a></li>            
+            <li><a href="#like" data-toggle="tab">我喜欢的文章</a></li>           
         </ul>
 
         <div id="myTabContent" class="tab-content">
@@ -38,13 +36,6 @@ $this->load->view('common/header');
             <div class="tab-pane fade" id="like">
                 <p>iOS 是一个由苹果公司开发和发布的手机操作系统。最初是于 2007 年首次发布 iPhone、iPod Touch 和 Apple
                     TV。iOS 派生自 OS X，它们共享 Darwin 基础。OS X 操作系统是用在苹果电脑上，iOS 是苹果的移动版本。</p>
-            </div>
-            <div class="tab-pane fade" id="follow">
-                <p>jMeter 是一款开源的测试软件。它是 100% 纯 Java 应用程序，用于负载和性能测试。</p>
-            </div>
-            <div class="tab-pane fade" id="fans">
-                <p>Enterprise Java Beans（EJB）是一个创建高度可扩展性和强大企业级应用程序的开发架构，部署在兼容应用程序服务器（比如 JBOSS、Web Logic 等）的 J2EE 上。
-                </p>
             </div>
         </div>
     </div>    
