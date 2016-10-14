36a27cb8da9b2d655071e26917ed4cfd9931a238
linxin <601630504@qq.com>
2016-10-14 18:37:12 +0800

    c

--- application/views/explore.php
+++ application/views/explore.php
@@ -2,38 +2,35 @@
 $this->load->view('common/header');
 ?>
 
-  <div class="container catelog_select" style="margin-top:80px;<?php if(!$myLikeCatelog):?>display:block;<?php endif;?>">
-      <p class="caltelog_select_sub">选取您感兴趣的分类，系统会根据您的选择做出文章推荐</p>
-      <div> 
-        <?php foreach($allCatelog as $v):?>
-        <span class="label <?php if(in_array($v['id'], $likeCatelogId)):?>label-success<?php else:?>label-default<?php endif;?>" data-id="<?php echo $v['id']?>">
-          <?php echo $v['name']?>
-          <?php if(in_array($v['id'], $likeCatelogId)):?> <i class="fa fa-check" ></i><?php endif;?>
-        </span>
-        <?php endforeach;?>
-      </div>
-      <div class="clearfix">
-        <button class="red-btn explore-btn" type="button" style="margin:10px;">๑❛ᴗ❛๑ 提交选择</button>
-      </div>
-  </div>
-<?php if($myLikeCatelog):?>
-<div class="container" style="margin-top:80px;">
+
+
+<div class="container" style="margin-top:60px;">
   <div class="row">
     <div class="span9">
+      <div class="catelog_select" <?php if($myLikeCatelog):?>style="display:none;"<?php endif;?>>
+          <p class="caltelog_select_sub">选取您感兴趣的分类，系统会根据您的选择做出文章推荐</p>
+          <div> 
+            <?php foreach($allCatelog as $v):?>
+            <span class="label <?php if(in_array($v['id'], $likeCatelogId)):?>label-success<?php else:?>label-default<?php endif;?>" data-id="<?php echo $v['id']?>">
+              <?php echo $v['name']?>
+              <?php if(in_array($v['id'], $likeCatelogId)):?> <i class="fa fa-check" ></i><?php endif;?>
+            </span>
+            <?php endforeach;?>
+          </div>
+          <div class="clearfix">
+            <button class="red-btn explore-btn" type="button" style="margin-left:10px;">๑❛ᴗ❛๑ 提交选择</button>
+          </div>
+      </div>
+      
     <?php if(empty($collect)):?>
-
     <?php else:?>
       <?php foreach($collect as $v):?>
         <div class="media">
-    
           <div class="media-body">
             <h4 class="media-heading"><a href=""><?php echo $v['title']?></a></h4>
             <?php echo $v['summary']?>
           </div>
           <div class="media-body" style="padding-left:60px;margin-top:8px;">
-            <span class="label label-success">Http</span>
-            <span class="label label-success">计算机技术</span>
-            <span class="label label-success">文本分析</span>
             <ul class="inline pull-right" style="display:inline;">
               <li class="action_favorite" title="收录到我的阅读"><a href=""><i class="fa fa-star-o"></i> 收录到我的阅读</a></li>
               <li class="action_share" title="分享"><a href=""><i class="fa fa-share"></i>分享</a></li>
@@ -43,9 +40,10 @@ $this->load->view('common/header');
       <?php endforeach;?>
     <?php endif;?>
       </div>
-    <div class="span3">...</div>
-  </div>
-<?php endif;?>        
+    <div class="span3">
+      <?php if($myLikeCatelog):?><a href=""><i class="fa fa-gear"></i> 发现阅读偏好设置</a><?php endif;?>
+    </div>
+  </div>     
 </div>
 
 <?php
